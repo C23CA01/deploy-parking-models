f8a4ee0099c44941100cd5d9ccf650319c69b169
C23CA01 <150264878+C23CA01@users.noreply.github.com>
2023-12-08 21:53:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ import tensorflow as tf
 from yolotf2.models import (
     YoloV3
 )
-from yolotf2.dataset import transform_images, load_tfrecord_dataset
+from yolotf2.dataset import transform_images
 # from yolotf2.utils import draw_outputs
-from flask import Flask, request, Response, jsonify, send_from_directory, abort,render_template
+from flask import Flask, request, jsonify, abort,render_template
 import os
 
 # customize your API through the following parameters
 classes_path = './data/labels/coco.names'
-weights_path = './YoloWeights/checkpoint/yolov3.tf'
+weights_path = './parkingmodel.h5'
 tiny = False                    # set to True if using a Yolov3 Tiny model
 size = 416                      # size images are resized to for model
 output_path = '/'   # path to output folder where images with detections are saved
@@ -24,7 +24,7 @@ num_classes = 1                # number of classes in model
 
 yolo = YoloV3(classes=num_classes)
 
-yolo.load_weights(weights_path).expect_partial()
+yolo.load_weights(weights_path)
 print('weights loaded')
 
 class_names = [c.strip() for c in open(classes_path).readlines()]
@@ -142,6 +142,8 @@ def get_detections():
                 connection.close()
                 print("Connection closed.")
                 response.append(response_entry)
+                
+        # debugging yolov3 utilities
         # img = cv2.cvtColor(raw_img.numpy(), cv2.COLOR_RGB2BGR)
         # img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         # cv2.imwrite(output_path + 'detection' + str(num) + '.jpg', img)
@@ -154,43 +156,5 @@ def get_detections():
         return jsonify({"response":response}), 200
     except FileNotFoundError:
         abort(404)
-
-# API that returns image with detections on it
-# @app.route('/image', methods= ['POST'])
-# def get_image():
-#     image = request.files["images"]
-#     image_name = image.filename
-#     image.save(os.path.join(os.getcwd(), image_name))
-#     img_raw = tf.image.decode_image(
-#         open(image_name, 'rb').read(), channels=3)
-#     img = tf.expand_dims(img_raw, 0)
-#     img = transform_images(img, size)
-
-#     t1 = time.time()
-#     boxes, scores, classes, nums = yolo(img)
-#     t2 = time.time()
-#     print('time: {}'.format(t2 - t1))
-
-#     print('detections:')
-#     for i in range(nums[0]):
-#         print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-#                                         np.array(scores[0][i]),
-#                                         np.array(boxes[0][i])))
-#     img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
-#     img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-#     cv2.imwrite(output_path + 'detection.jpg', img)
-#     print('output saved to: {}'.format(output_path + 'detection.jpg'))
-    
-#     # prepare image for response
-#     _, img_encoded = cv2.imencode('.png', img)
-#     response = img_encoded.tostring()
-    
-#     #remove temporary image
-#     os.remove(image_name)
-
-#     try:
-#         return Response(response=response, status=200, mimetype='image/png')
-#     except FileNotFoundError:
-#         abort(404)
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0', port=5000)
