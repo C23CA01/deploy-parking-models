97f97eb1656ad6c5386cebe977bd2677e38c15cc
C23CA01 <150264878+C23CA01@users.noreply.github.com>
2023-12-12 17:43:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def get_detections():
     except FileNotFoundError:
         abort(404)
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5000)
+    app.run(debug=False, host = '0.0.0.0', port=5000)
